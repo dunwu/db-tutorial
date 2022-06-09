61edc8b97d29c0b49de3e92e57568713bc4391b3
dunwu <forbreak@163.com>
2022-06-09 15:16:02 +0800

    feat: 更新文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,15 +12,15 @@ module.exports = {
     // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
-    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    ['meta', { name: 'theme-color', content: '#11a8cd' }] // 移动浏览器主题颜色
   ],
   markdown: {
     // lineNumbers: true,
     extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
     externalLinks: {
       target: '_blank',
-      rel: 'noopener noreferrer',
-    },
+      rel: 'noopener noreferrer'
+    }
   },
   // 主题配置
   themeConfig: {
@@ -33,24 +33,24 @@ module.exports = {
         items: [
           { text: '综合', link: '/03.关系型数据库/01.综合/' },
           { text: 'Mysql', link: '/03.关系型数据库/02.Mysql/' },
-          { text: '其他', link: '/03.关系型数据库/99.其他/' },
-        ],
+          { text: '其他', link: '/03.关系型数据库/99.其他/' }
+        ]
       },
       {
         text: '文档数据库',
-        items: [{ text: 'MongoDB', link: '/04.文档数据库/01.MongoDB/' }],
+        items: [{ text: 'MongoDB', link: '/04.文档数据库/01.MongoDB/' }]
       },
       {
         text: 'KV数据库',
-        items: [{ text: 'Redis', link: '/05.KV数据库/01.Redis/' }],
+        items: [{ text: 'Redis', link: '/05.KV数据库/01.Redis/' }]
       },
       {
         text: '搜索引擎数据库',
         items: [
           { text: 'Elasticsearch', link: '/07.搜索引擎数据库/01.Elasticsearch/' },
-          { text: 'Elastic技术栈', link: '/07.搜索引擎数据库/02.Elastic/' },
-        ],
-      },
+          { text: 'Elastic技术栈', link: '/07.搜索引擎数据库/02.Elastic/' }
+        ]
+      }
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: 'https://raw.githubusercontent.com/dunwu/images/dev/common/dunwu-logo.png', // 导航栏logo
@@ -68,7 +68,7 @@ module.exports = {
     // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
     updateBar: {
       // 最近更新栏
-      showToArticle: true, // 显示到文章页底部，默认true
+      showToArticle: true // 显示到文章页底部，默认true
       // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
     },
     // titleBadge: false, // 文章标题前的图标是否显示，默认true
@@ -93,7 +93,7 @@ module.exports = {
     author: {
       // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
       name: 'dunwu', // 必需
-      href: 'https://github.com/dunwu', // 可选的
+      href: 'https://github.com/dunwu' // 可选的
     },
     social: {
       // 社交图标，显示于博主信息栏和页脚栏
@@ -102,21 +102,21 @@ module.exports = {
         {
           iconClass: 'icon-youjian',
           title: '发邮件',
-          link: 'mailto:forbreak@163.com',
+          link: 'mailto:forbreak@163.com'
         },
         {
           iconClass: 'icon-github',
           title: 'GitHub',
-          link: 'https://github.com/dunwu',
-        },
-      ],
+          link: 'https://github.com/dunwu'
+        }
+      ]
     },
     footer: {
       // 页脚信息
       createYear: 2019, // 博客创建年份
-      copyrightInfo: '钝悟（dunwu） | CC-BY-SA-4.0', // 博客版权信息，支持a标签
+      copyrightInfo: '钝悟（dunwu） | CC-BY-SA-4.0' // 博客版权信息，支持a标签
     },
-    htmlModules,
+    htmlModules
   },
 
   // 插件
@@ -126,8 +126,8 @@ module.exports = {
       {
         // 鼠标点击爱心特效
         color: '#11a8cd', // 爱心颜色，默认随机色
-        excludeClassName: 'theme-vdoing-content', // 要排除元素的class, 默认空''
-      },
+        excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
+      }
     ],
 
     ['fulltext-search'], // 全文搜索
@@ -157,8 +157,8 @@ module.exports = {
         copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
         copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
         duration: 1000, // prompt message display time.
-        showInMobile: false, // whether to display on the mobile side, default: false.
-      },
+        showInMobile: false // whether to display on the mobile side, default: false.
+      }
     ],
     [
       'demo-block',
@@ -170,18 +170,18 @@ module.exports = {
           // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
           jsfiddle: false, // 是否显示 jsfiddle 链接
           codepen: true, // 是否显示 codepen 链接
-          horizontal: false, // 是否展示为横向样式
-        },
-      },
+          horizontal: false // 是否展示为横向样式
+        }
+      }
     ],
     [
       'vuepress-plugin-zooming', // 放大图片
       {
         selector: '.theme-vdoing-content img:not(.no-zoom)',
         options: {
-          bgColor: 'rgba(0,0,0,0.6)',
-        },
-      },
+          bgColor: 'rgba(0,0,0,0.6)'
+        }
+      }
     ],
     [
       '@vuepress/last-updated', // "上次更新"时间格式
@@ -189,11 +189,30 @@ module.exports = {
         transformer: (timestamp, lang) => {
           const dayjs = require('dayjs') // https://day.js.org/
           return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
-        },
-      },
+        }
+      }
     ],
+    [
+      'vuepress-plugin-comment', // 评论
+      {
+        choosen: 'gitalk',
+        options: {
+          clientID: '7dd8c87a20cff437d2ed',
+          clientSecret: '4e28d81a9a0280796b2b45ce2944424c6f2c1531',
+          repo: 'db-tutorial', // GitHub 仓库
+          owner: 'dunwu', // GitHub仓库所有者
+          admin: ['dunwu'], // 对仓库有写权限的人
+          // distractionFreeMode: true,
+          pagerDirection: 'last', // 'first'正序 | 'last'倒序
+          id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
+          title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
+          labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
+          body: '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>' // GitHub issue 的内容
+        }
+      }
+    ]
   ],
 
   // 监听文件变化并重新构建
-  extraWatchFiles: ['.vuepress/config.js', '.vuepress/config/htmlModules.js'],
+  extraWatchFiles: ['.vuepress/config.js', '.vuepress/config/htmlModules.js']
 }
---
+++ docs/01.计算机科学/02.数据库/07.搜索引擎数据库/01.Elasticsearch/README.md
@@ -0,0 +1,78 @@
+---
+title: Elasticsearch 教程
+date: 2022-04-11 16:52:35
+categories:
+  - 计算机科学
+  - 数据库
+  - 搜索引擎数据库
+  - Elasticsearch
+tags:
+  - 数据库
+  - 搜索引擎数据库
+  - Elasticsearch
+permalink: /pages/74675e/
+hidden: true
+---
+
+# Elasticsearch 教程
+
+> Elasticsearch 是一个基于 Lucene 的搜索和数据分析工具，它提供了一个分布式服务。Elasticsearch 是遵从 Apache 开源条款的一款开源产品，是当前主流的企业级搜索引擎。
+
+## 📖 内容
+
+### [Elasticsearch 面试总结](01.Elasticsearch面试总结.md) 💯
+
+### [Elasticsearch 快速入门](02.Elasticsearch快速入门.md)
+
+### [Elasticsearch 简介](03.Elasticsearch简介.md)
+
+### [Elasticsearch 索引管理](04.Elasticsearch索引.md)
+
+### [Elasticsearch 映射](05.Elasticsearch映射.md)
+
+### [Elasticsearch 查询](05.Elasticsearch查询.md)
+
+### [Elasticsearch 高亮](06.Elasticsearch高亮.md)
+
+### [Elasticsearch 排序](07.Elasticsearch排序.md)
+
+### [Elasticsearch 聚合](08.Elasticsearch聚合.md)
+
+### [Elasticsearch 分析器](09.Elasticsearch分析器.md)
+
+### [Elasticsearch 性能优化](10.Elasticsearch性能优化.md)
+
+### [Elasticsearch Rest API](11.ElasticsearchRestApi.md)
+
+### [ElasticSearch Java API 之 High Level REST Client](12.ElasticsearchHighLevelRestJavaApi.md)
+
+### [Elasticsearch 集群和分片](13.Elasticsearch集群和分片.md)
+
+### [Elasticsearch 运维](20.Elasticsearch运维.md)
+
+## 📚 资料
+
+- **官方**
+  - [Elasticsearch 官网](https://www.elastic.co/cn/products/elasticsearch)
+  - [Elasticsearch Github](https://github.com/elastic/elasticsearch)
+  - [Elasticsearch 官方文档](https://www.elastic.co/guide/en/elasticsearch/reference/current/index.html)
+  - [Elasticsearch: The Definitive Guide](https://www.elastic.co/guide/en/elasticsearch/guide/master/index.html) - ElasticSearch 官方学习资料
+- **书籍**
+  - [《Elasticsearch 实战》](https://book.douban.com/subject/30380439/)
+- **教程**
+  - [ELK Stack 权威指南](https://github.com/chenryn/logstash-best-practice-cn)
+  - [Elasticsearch 教程](https://www.knowledgedict.com/tutorial/elasticsearch-intro.html)
+- **文章**
+  - [Elasticsearch+Logstash+Kibana 教程](https://www.cnblogs.com/xing901022/p/4704319.html)
+  - [ELK（Elasticsearch、Logstash、Kibana）安装和配置](https://github.com/judasn/Linux-Tutorial/blob/master/ELK-Install-And-Settings.md)
+  - **性能调优相关**的工程实践
+    - [Elasticsearch Performance Tuning Practice at eBay](https://www.ebayinc.com/stories/blogs/tech/elasticsearch-performance-tuning-practice-at-ebay/)
+    - [Elasticsearch at Kickstarter](https://kickstarter.engineering/elasticsearch-at-kickstarter-db3c487887fc)
+    - [9 tips on ElasticSearch configuration for high performance](https://www.loggly.com/blog/nine-tips-configuring-elasticsearch-for-high-performance/)
+    - [Elasticsearch In Production - Deployment Best Practices](https://medium.com/@abhidrona/elasticsearch-deployment-best-practices-d6c1323b25d7)
+- **更多资源**
+  - [GitHub: Awesome ElasticSearch](https://github.com/dzharii/awesome-elasticsearch)
+
+## 🚪 传送
+
+◾ 💧 [我的 IT 知识图谱](https://dunwu.github.io/waterdrop/) ◾ 🎯 [我的博客](https://dunwu.github.io/blog/) ◾
